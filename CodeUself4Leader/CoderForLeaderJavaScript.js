201b6b138db24bd74b46a8124b4bf27353d5ba05
Zen Master <LiliyaFrye@gmail.com>
2019-10-22 20:44:58 -0700

    upload files

---
+++ CodeUself4Leader/CoderForLeaderJavaScript.js
@@ -0,0 +1,21 @@
+//JAVASCRIPT:
+let traits = [,
+    'Good coaches',
+    'Bring the best in people',
+    'Good communicators',
+    'Adaptable and influential',
+    'Always focus on the user / customer / client',
+    'Have clear vision and strategy for the team',
+    'Empower and inspire the team with a true sense of purpose',
+    'Encourage the discussion of ideas to ensure that people feel involved and have a voice',
+    'Provide trust and freedom to the team members',
+    'Productive and result-oriented',
+    'Inspire healthy learning from failures and avoid finger pointing',
+    'Assist others with career development and gaining more knowledge',
+    'Express sincere interest for team members’ success and personal well-being',
+    'Have important technical skills to advice the team and never stop learning',
+];
+
+traits.forEach(function(value, index) {
+    console.log(index + ". " + value);
+});
